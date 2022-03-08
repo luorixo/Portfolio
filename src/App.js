b4f59a0c3c71d1aae6e69b1cc1829bbdc4cca577
luorixo <luorixo@gmail.com>
2022-03-08 13:20:37 +1300

    added logos and home button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import { darkTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
+// components
+import Main from './components/Main';
+import About from './components/About';
+import Blog from './components/Blog';
+import Work from './components/Work';
+import Skills from './components/Skills';
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
-}
+  return <>
+    <GlobalStyle/>
+
+      <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/blog" component={Blog}/>
+        <Route exact path="/work" component={Work}/>
+        <Route exact path="/skills" component={Skills}/>
+      </Switch>
+      </ThemeProvider>
+  </> 
+} 
 
 export default App
-
---
+++ src/globalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+// global app styling
+*, *::before, *::after, h1, h2, h3, h4, h5, h6 {
+    padding: 0;
+    margin: 0;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    display: inline-block;
+}
+
+body {
+    padding: 0;
+    margin: 0;
+    overflow-x: hidden;
+    font-family: 'Source Sans Pro', sans-serif;
+}
+`
+
+export default GlobalStyle;
+
+
