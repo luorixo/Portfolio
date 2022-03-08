7e6576d874c08806e3293d1e0f94b84e5fdf06a0
luorixo <luorixo@gmail.com>
2022-03-08 21:53:03 +1300

    homepage styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import React, { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
 
 import PowerButton from '../subComponents/PowerButton'
 import LogoComponent from '../subComponents/LogoComponent'
 import SocialIcons from '../subComponents/SocialIcons'
+import { NavLink } from 'react-router-dom'
+import { YinYang } from './AllSvgs'
 
 const MainContainer = styled.div`
 background: ${props => props.theme.body};
@@ -21,14 +24,153 @@ h1, h2, h3, h4, h5, h6 {
 const Container = styled.div`
 padding: 2rem;
 `
+const Contact = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 2rem;
+right: calc(1rem + 2vw);
+text-decoration: none;
+z-index: 1;
+`
+
+const Blog = styled(NavLink)`
+color: ${props => props.theme.text};
+position: absolute;
+top: 50%;
+right: calc(1rem + 2vw);
+transform: rotate(90deg) translate(-50%, -50%);
+text-decoration: none;
+z-index: 1;
+`
+
+const Work = styled(NavLink)`
+color: ${props => props.click ? props.theme.body : props.theme.text};
+position: absolute;
+top: 50%;
+left: calc(1rem + 2vw);
+transform: translate(-50%, -50%) rotate(-90deg);
+text-decoration: none;
+z-index: 1;
+transition: ease-out 0.4s;
+`
+
+const BottomBar = styled.div`
+position: absolute;
+bottom: 2rem;
+left: 0;
+right: 0;
+width: 100%;
+
+display: flex;
+justify-content: space-evenly;
+`
+
+const About = styled(NavLink)`
+color: ${props => props.click ? props.theme.body : props.theme.text};
+text-decoration: none;
+z-index: 1;
+transition: ease-out 0.4s;
+`
+
+const Skills = styled(NavLink)`
+color: ${props => props.theme.text};
+text-decoration: none;
+z-index: 1;
+`
+const rotate = keyframes`
+from {
+    transform: rotate(0);
+}
+to {
+    transform: rotate(360deg);
+}
+`
+
+const Center = styled.button`
+position: absolute;
+top: ${props => props.click ? '85%' :'50%'  };
+left: ${props => props.click ? '92%' :'50%'  };
+transform: translate(-50%, -50%);
+border: none;
+outline: none;
+background-color: transparent;
+cursor: pointer;
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+transition: all 0.8s ease;
+
+&>:first-child {
+    animation: ${rotate} infinite 4s linear;
+}
+
+&>:last-child {
+    display: ${props => props.click ? 'none' : 'inline-block' };
+    padding-top: 1rem;
+}
+`
+const DarkDiv = styled.div`
+position: absolute;
+background-color: #000;
+top: 0;
+bottom: 0;
+right: 50%;
+width: ${props => props.click ? '50%' : '0%'};
+height: ${props => props.click ? '100%' : '0%'};
+z-index: 1;
+transition: height 0.5s ease, width 1s ease 0.5s
+`
 
 const Main = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
         <MainContainer>
+        <DarkDiv click={click}/>
             <Container>
                 <PowerButton/>
-                <LogoComponent/>
-                <SocialIcons/>
+                <LogoComponent theme={click ? 'dark' : 'light'}/>
+                <SocialIcons theme={click ? 'dark' : 'light'}/>
+
+                <Center click={click}>
+                    <YinYang  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
+                    <span>click here</span>
+                </Center>
+
+                <Contact target="_blank" to={{pathname:"mailto:luorixo@gmail.com"}}>
+                    <h2>
+                        Say hi...
+                    </h2>
+                </Contact>
+                <Blog to={{pathname:"/blog"}}>
+                    <h2>
+                        Blog
+                    </h2>
+                </Blog>
+                <Work to={{pathname:"/work"}} click={click}>
+                    <h2>
+                        Work
+                    </h2>
+                </Work>
+
+                <BottomBar>
+                <About to={{pathname:"/about"}} click={click}>
+                    <h2>
+                        About.
+                    </h2>
+                </About>
+                <Skills to={{pathname:"/skills"}}>
+                    <h2>
+                        Skills.
+                    </h2>
+                </Skills>
+                </BottomBar>
+                
+
             </Container>
         </MainContainer>
     )
--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Facebook, Github, Twitter, YouTube } from '../components/AllSvgs';
+import {darkTheme} from '../components/Themes'
 
 const Icons = styled.div`
 display: flex;
@@ -22,33 +23,33 @@ z-index: 3;
 const Line = styled.span`
 width: 2px;
 height: 8rem;
-background-color: ${props => props.theme.text};
+background-color: ${props => props.color === 'dark' ? darkTheme.text : darkTheme.body };
 `
 
-const SocialIcons = () => {
+const SocialIcons = (props) => {
     return (
         <Icons>
             <div>
                 <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://github.com/luorixo"}}>
-                    <Github width={25} height={25} fill='currentColor'/>
+                    <Github width={25} height={25} fill={props.theme === "dark" ? darkTheme.text : darkTheme.body }/>
                 </NavLink>
             </div>
             <div>
                 <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://github.com/luorixo"}}>
-                    <Facebook width={25} height={25} fill='currentColor'/>
+                    <Facebook width={25} height={25} fill={props.theme === "dark" ? darkTheme.text : darkTheme.body }/>
                 </NavLink>
             </div>
             <div>
                 <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://github.com/luorixo"}}>
-                    <Twitter width={25} height={25} fill='currentColor'/>
+                    <Twitter width={25} height={25} fill={props.theme === "dark" ? darkTheme.text : darkTheme.body }/>
                 </NavLink>
             </div>
             <div>
                 <NavLink style={{color:'inherit'}} target="_blank" to={{pathname:"https://github.com/luorixo"}}>
-                    <YouTube width={25} height={25} fill='currentColor'/>
+                    <YouTube width={25} height={25} fill={props.theme === "dark" ? darkTheme.text : darkTheme.body }/>
                 </NavLink>
             </div>
-            <Line/>
+            <Line color={props.theme}/>
         </Icons>  
     )
 }
