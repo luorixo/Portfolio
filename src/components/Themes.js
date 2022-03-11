8fe539ad9d98cd5565b7b9d3522759ff79183c40
luorixo <luorixo@gmail.com>
2022-03-11 16:53:17 +1300

    dark background trial

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -7,16 +7,16 @@
 
 
 export const lightTheme = {
-    body:"#fffae0",
-    text:"#631F0F",
+    body:"#222",
+    text:"#C2DC71",
     fontFamily:"'Source Sans Pro', sans-serif",
     bodyRgba : "252, 246, 244",
     textRgba:"0,0,0",
 }
 
 export const darkTheme = {
-    body:"#631F0F",
-    text:"#fffae0",
+    body:"#C2DC71",
+    text:"#222",
     fontFamily:"'Source Sans Pro', sans-serif",
     bodyRgba : "0,0,0",
     textRgba:"252, 246, 244",
