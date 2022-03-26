15cedba136a307d1b6b720f6575ef61318524d39
luorixo <luorixo@gmail.com>
2022-03-26 13:58:42 +1300

    changes

---
+++ src/components/Main.js
@@ -0,0 +1,233 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import React, { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+
+// background particles
+import Particles from 'react-particles-js';
+import configLayerOne from "../config/particlesjs-config-layer-one.json"
+import configLayerTwo from "../config/particlesjs-config-layer-two.json"
+import configStars from "../config/particlesjs-config-stars.json"
+import configFeathers from "../config/particlesjs-config.json"
+
+import LogoComponent from '../subComponents/LogoComponent'
+import ContactComponent from '../subComponents/Contact';
+import SocialIcons from '../subComponents/SocialIcons'
+import { NavLink } from 'react-router-dom'
+import { GreenBird } from './AllSvgs'
+import Intro from './Intro'
+
+const Box = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+left: 0;
+bottom: 0;
+z-index: 1;
+`
+
+const MainContainer = styled(motion.div)`
+background: ${props => props.theme.body};
+background: #102F42;
+background: #007859;
+/*background-image: url();
+background-size: cover;
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;*/
+width: 100vw;
+height: 100%;
+overflow: hidden;
+position: fixed;
+z-index: 10;
+
+
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Karla', sans-serif;
+    font-weight: 500;
+}
+`
+
+const Container = styled.div`
+padding: 20rem;
+left: 20%;
+`
+
+const BottomBar = styled.div`
+position: absolute;
+bottom: 2rem;
+/*background: #fff;
+padding: 1rem;*/
+left: 0;
+right: 0;
+width: 100%;
+
+display: flex;
+justify-content: space-evenly;
+`
+
+const About = styled(NavLink)`
+color: ${props => props.theme.body};
+text-decoration: none;
+z-index: 1;
+transition: ease-out 0.4s;
+`
+
+const Projects = styled(NavLink)`
+color: ${props => props.theme.body};
+text-decoration: none;
+z-index: 1;
+`
+const bounce = keyframes`
+0% {
+    /*transform: rotate(0);*/
+    transform: translate(0, 0);
+}
+
+20% {
+    transform: translate(0, 2px);
+}
+
+50% {
+    /*transform: rotate(0);*/
+    transform: translate(0, 23px);
+}
+
+60% {
+    transform: translate(0, 6px);
+}
+
+80% {
+    transform: translate(0, 2px);
+}
+
+100% {
+    /*transform: rotate(360deg);*/
+    transform: translate(0, 0);
+}
+`
+
+const Center = styled.button`
+position: absolute;
+top: ${props => props.click ? '85%' :'50%'  };
+left: ${props => props.click ? '92%' :'50%'  };
+transform: translate(-50%, -50%);
+border: none;
+outline: none;
+background-color: transparent;
+-webkit-tap-highlight-color: transparent;
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+transition: all 1s ease;
+z-index: 10;
+
+&>:first-child {
+    animation: ${bounce} infinite 1.1s linear;
+}
+
+&>:last-child {
+    display: ${props => props.click ? 'none' : 'inline-block' };
+    padding-top: 1rem;
+}
+`
+const DarkDiv = styled.div`
+position: absolute;
+background-color: #C2DC71;
+top: 0;
+bottom: 0;
+right: 0%;
+width: ${props => props.click ? '100%' : '0%'};
+height: ${props => props.click ? '100%' : '1%'};
+transition: height 0.5s ease, width 1s ease 0.5s;
+z-index: 1;
+`
+
+// framer-motion config
+const container = {
+    hidden: {opacity:0},
+    show: {
+        opacity: 1,
+        transition: {
+            duration: 0.25,
+        }
+    }
+}
+
+const Main = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+    return (
+        <MainContainer variants={container}
+        initial='hidden'
+        animate='show'
+        exit={{
+            opacity:0, transition:{duration: 0.25}
+        }}>
+        
+        <DarkDiv click={click}/>
+        {click ? <LogoComponent theme={'dark'}/> : null }
+            <Container>
+                <Box>
+                    {/*<Particles style={{position:'absolute',top:0}} params={configStars}/>
+                    <Particles style={{position:'absolute',top:0}} params={configLayerTwo}/>
+                    <Particles style={{position:'absolute',top:0}} params={configLayerOne}/>*/}
+                    <Particles style={{position:'absolute',top:0}} params={configFeathers}/>
+                </Box>
+                
+                {click ? <SocialIcons style={{transition:'ease-out 0.4s'}} theme={'dark'}/> : null }
+
+                <Center click={click}>
+                    <GreenBird style={{cursor:'pointer'}} onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' alt='clickable green bird icon'/>
+                    <span style={{color:'#fff'}}>click me ↑</span>
+                </Center>
+
+                {click ? <ContactComponent/>: null }
+                
+                {click ? <BottomBar>
+                <About to={{pathname:"/about"}}>
+                    <motion.h2
+                    initial={{
+                        y:200,
+                        transition: { type:'spring', duration: 1.5, delay:0}
+                    }}
+                    animate={{
+                        y:0,
+                        transition: { type:'spring', bounce:0.4, duration: 2, delay:1.6}
+                    }}
+                    whileHover={{scale: 1.15}}
+                    whileTap={{scale: 0.9}}
+                    >
+                        ➪ About.
+                    </motion.h2>
+                </About>
+                <Projects to={{pathname:"/projects"}}>
+                    <motion.h2
+                    initial={{
+                        y:200,
+                        transition: { type:'spring', duration: 1.5, delay:0}
+                    }}
+                    animate={{
+                        y:0,
+                        transition: { type:'spring', bounce:0.4, duration: 2, delay:1.7}
+                    }}
+                    whileHover={{scale: 1.15}}
+                    whileTap={{scale: 0.9}}
+                    >
+                        ➪ My Projects.
+                    </motion.h2>
+                </Projects>
+                </BottomBar> : null }
+                
+            </Container>
+            
+            {click ? <Intro/> : null }
+            
+        </MainContainer>
+    )
+}
+
+export default Main
---
+++ src/components/Projects.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { lightTheme } from './Themes'
+import { Design, Develope } from './AllSvgs'
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
+import ParticleComponent from '../subComponents/ParticleComponent'
+import { motion } from 'framer-motion';
+import ContactComponent from '../subComponents/Contact'
+
+const Box = styled(motion.div)`
+background-color: ${props => props.theme.body};
+
+background-color: ${props => props.theme.text};
+background-color: #C2DC71;
+width: 100vw;
+height: 100%;
+position: fixed;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+
+const Main = styled(motion.ul)`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+
+font-family: 'Ubuntu Mono', monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+
+&>*
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5em 0;
+
+strong {
+    marigin-bottom: 1rem;
+    text-transform: uppercase;
+}
+
+ul, p {
+    margin-left: 2rem;   
+}
+`
+
+// framer-motion config
+const container = {
+    hidden: {opacity:0},
+    show: {
+        opacity: 1,
+        transition: {
+            duration: 0.25,
+        }
+    }
+}
+
+const Projects = () => {
+    return (
+        <Box variants={container}
+        initial='hidden'
+        animate='show'
+        exit={{
+            opacity:0, transition:{duration: 0.25}
+        }}>
+            <LogoComponent theme={'dark'}/> 
+            <ContactComponent/>
+            <SocialIcons theme='dark'/>
+            <ParticleComponent theme='dark'/>
+            
+        </Box>
+    )
+}
+
+export default Projects
